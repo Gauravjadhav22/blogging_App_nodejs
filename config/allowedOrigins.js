1f9146dc1c53c722126861f3b6eaafb19b92562d
gaurav jadhav <gauravjadhav7512@gmail.com>
2023-05-02 15:56:48 +0530

    api changed

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'https://bloggin-app-frontend-react.herokuapp.com',
     'https://blogging-app-react-b5zomog0g-rgaurav22ak-gmailcom.vercel.app',
-    'https://blogging-app-react-3k80mwdeg-rgaurav22ak-gmailcom.vercel.app/',
+    'https://blogging-app-react-3k80mwdeg-rgaurav22ak-gmailcom.vercel.app',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000',
